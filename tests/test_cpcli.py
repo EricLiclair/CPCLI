1cf0d7e7ef9f04f77edb66d7c8dcc76ae451b134
Saurabh Kushwaha <1ms22cs126@msrit.edu>
2023-10-29 01:49:52 +0530

    Added : tests

--- tests/test_cpcli.py
+++ tests/test_cpcli.py
@@ -1,91 +1,84 @@
 import os
 import shutil
-import pytest
-from ..cpcli.cpcli import (
-    _create_file_from_template, _delete_directory, _file_exists,
-    clean_file_name, get_paths
-)
+import unittest
+
+from cpcli.cpcli import (_create_file_from_template, _delete_directory,
+                           _file_exists, clean_file_name, get_paths)
 
 class Base:
     test_file_name_1 = "test_file_name_1"
-    test_file_name_2 = "test_file_name_2"
 
-class TestMisc(Base):
+class TestMisc(unittest.TestCase):
 
     def test_get_paths(self):
-        dir_path, file_path, output_path = get_paths(self.test_file_name_1)
-        assert os.path.exists(dir_path)
-        assert os.path.exists(file_path)
-        assert os.path.exists(output_path)
+        dir_path = os.path.join(self.test_file_name_1)
+        file_path = f"{dir_path}/{self.test_file_name_1}.cpp"
+        output_path = f"{dir_path}/output.exe"
+        self.assertEqual(get_paths(self.test_file_name_1), (dir_path, file_path, output_path))
 
     def test_clean_file_name(self):
-        file_name_with_slash = self.test_file_name_1 + "/"
-        cleaned_file_name = clean_file_name(file_name_with_slash)
-        assert cleaned_file_name == self.test_file_name_1
+        _test_file_name_1 = self.test_file_name_1 + "/"
+        cleaned_file_name = _test_file_name_1.strip('/')
+        self.assertEqual(clean_file_name(_test_file_name_1), cleaned_file_name)
 
     def test_file_exists(self):
-        # Ensure the file does not exist initially
-        assert not _file_exists(self.test_file_name_1)
-
-        # Create a temporary test file
+        # create a temporary test file
         dir_path, file_path, _ = get_paths(self.test_file_name_1)
         os.mkdir(dir_path)
         file = open(file_path, 'w')
         file.close()
-
-        # Now, the file should exist
-        assert _file_exists(self.test_file_name_1)
-
-        # Delete the file and test directory
+        exception_file_path = _create_file_from_template(self.test_file_name_1)
+        self.assertIsNone(exception_file_path)
+        self.assertTrue(_file_exists(self.test_file_name_1))
+        # delete file path
         shutil.rmtree(dir_path)
 
-    def test_non_existing_file(self):
-        # Ensure that a non-existing file does not exist
-        assert not _file_exists("non_existing_file")
 
 class TestCreate(Base):
 
     def test_create_file_from_template(self):
-        # Test that the file is successfully created from the template
         file_path = _create_file_from_template(self.test_file_name_1)
-        assert os.path.isfile(file_path)
-
-        # Cleanup: Delete the test directory
+        self.assertIsNotNone(file_path)
+        self.assertTrue(os.path.isfile(file_path))
+        # delete file path
         shutil.rmtree(self.test_file_name_1)
 
-    def test_create_file_already_exists(self):
-        # Test creating a file that already exists
-        # Create the file first
-        _create_file_from_template(self.test_file_name_1)
-
-        # Attempt to create it again and check for a FileExistsError
-        with pytest.raises(FileExistsError):
-            _create_file_from_template(self.test_file_name_1)
 
 class TestDelete(Base):
 
     def test_delete_directory(self):
-        # Create a test file and directory
         file_path = _create_file_from_template(self.test_file_name_1)
-
-        # Verify that the file exists
-        assert os.path.isfile(file_path)
-
-        # Delete the test directory
         _delete_directory(self.test_file_name_1)
+        self.assertFalse(os.path.isfile(file_path))
 
-        # Verify that the file no longer exists after deletion
-        assert not os.path.exists(file_path)
+class TestAdditional(unittest.TestCase):
 
-    def test_delete_non_existing_directory(self):
-        # Test deleting a non-existing directory
-        # Ensure that the directory does not exist initially
-        assert not os.path.exists(self.test_file_name_2)
+    def test_create_file_from_template_with_existing_file(self):
+        # Test creating a file when a file with the same name already exists
+        dir_path, file_path, _ = get_paths(self.test_file_name_1)
+        os.mkdir(dir_path)
+        file = open(file_path, 'w')
+        file.close()
+        exception_file_path = _create_file_from_template(self.test_file_name_1)
+        self.assertIsNone(exception_file_path)
+        self.assertTrue(_file_exists(self.test_file_name_1))
+        # Clean up
+        shutil.rmtree(dir_path)
 
-        # Attempt to delete it and check for a FileNotFoundError
-        with pytest.raises(FileNotFoundError):
-            _delete_directory(self.test_file_name_2)
+    def test_delete_nonexistent_directory(self):
+        # Test deleting a directory that doesn't exist
+        non_existent_dir = "non_existent_directory"
+        self.assertFalse(os.path.exists(non_existent_dir))
+        _delete_directory(non_existent_dir)  # This should not raise an error
 
-# Run the tests
+    def test_file_does_not_exist(self):
+        # Test the _file_exists function with a file that doesn't exist
+        self.assertFalse(_file_exists("non_existent_file"))
+
+    def test_clean_file_name_no_slash(self):
+        # Test the clean_file_name function with a filename that doesn't contain slashes
+        file_name = "test_file_name_2.cpp"
+        self.assertEqual(clean_file_name(file_name), file_name)
+
 if __name__ == '__main__':
-    pytest.main()
+    unittest.main()
